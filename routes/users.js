092c2b1cfc4d6a0ae7c920e26772c0206a5246f3
jodch <josedavidchiquillo@gmail.com>
2020-05-18 21:08:13 -0300

    mailconfig

--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,8 @@ const email = (email, content) => {
     const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.USER_EMAIL,
-      pass: process.env.USER_PASSWORD,
+      user: 'undertheskydeco024@gmail.com',
+      pass: 'Bajoelcielo1-',
     },
     tls: {
       rejectUnauthorized: false,
